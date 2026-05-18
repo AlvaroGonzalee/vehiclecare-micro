dab14ff9a38daff3a56006ece8037feb80458818
alvarogonzale <a.gonzale.ortega@gmail.com>
2026-05-18 19:12:14 +0200

    Add EntityGraph annotations to MaintenanceRecordJpaRepository for optimized fetching of attachments

--- src/main/java/com/vehiclecare/vehiclecaremicro/infrastructure/persistence/repository/MaintenanceRecordJpaRepository.java
+++ src/main/java/com/vehiclecare/vehiclecaremicro/infrastructure/persistence/repository/MaintenanceRecordJpaRepository.java
@@ -1,13 +1,20 @@
 package com.vehiclecare.vehiclecaremicro.infrastructure.persistence.repository;
 
 import com.vehiclecare.vehiclecaremicro.infrastructure.persistence.entity.MaintenanceRecordEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MaintenanceRecordJpaRepository extends JpaRepository<MaintenanceRecordEntity, String> {
+
+    @EntityGraph(attributePaths = "attachments")
     List<MaintenanceRecordEntity> findByVehicle_Id(String vehicleId);
+
+    @EntityGraph(attributePaths = "attachments")
     List<MaintenanceRecordEntity> findByVehicle_IdAndVehicle_User_Id(String vehicleId, String userId);
+
+    @EntityGraph(attributePaths = "attachments")
     Optional<MaintenanceRecordEntity> findByIdAndVehicle_User_Id(String id, String userId);
 }
